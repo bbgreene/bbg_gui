5431c775a283a8f3ba768aecf98796eec230fb05
bbgreene <brian.greene.music@gmail.com>
2022-05-20 13:44:31 +0200

    checked customisability of text buttons/toggles

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,7 +3,7 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
-    // 4 example dials. The colours of each can be edited here to override initProperties() in dial.cpp/h
+    // 4 example dials. The colours of each can be edited here to override initProperties() in Dial.cpp/h
     
     //Example of Live style dial...examples of changing the colours of certain elements of dial.
     gain.setDialStyle(bbg_gui::bbg_Dial::DialStyle::kDialLiveStyle);
@@ -17,15 +17,23 @@ MainComponent::MainComponent()
     balance.setDialStyle(bbg_gui::bbg_Dial::DialStyle::kDialDotStyle);
     addAndMakeVisible(balance);
     
-    //Example of modern style dial with text box/label in middle. Customisable colours used to override initProperties() in dial.cpp/h
+    //Example of modern style dial with text box/label in middle. Customisable colours used to override initProperties() in Dial.cpp/h
     depth.setDialStyle(bbg_gui::bbg_Dial::DialStyle::kDialModernStyle);
     depth.setColour(juce::Slider::ColourIds::rotarySliderOutlineColourId, CustomColours::blackGrey);
     depth.setColour(juce::Slider::ColourIds::thumbColourId, CustomColours::creamWhite);
     addAndMakeVisible(depth);
     
+    // 2 example buttons. The colours of each can be edited here to override initProperties() in Toggle.cpp/h
+    
+    //Example of toggle number one with text in middle. Customisable colours used to override initProperties() in Toggle.cpp/h
     phase.setToggleStyle(bbg_gui::bbg_Toggle::ToggleStyle::kToggleOne);
+    phase.setColour(juce::TextButton::ColourIds::buttonColourId, CustomColours::blackGrey);
+    phase.setColour(juce::TextButton::ColourIds::buttonOnColourId, CustomColours::creamWhite);
+    phase.setColour(juce::TextButton::ColourIds::textColourOnId, CustomColours::blackGrey);
+    phase.setColour(juce::TextButton::ColourIds::textColourOffId, CustomColours::creamWhite);
     addAndMakeVisible(phase);
     
+    //Example of toggle number two with text in middle.
     mute.setToggleStyle(bbg_gui::bbg_Toggle::ToggleStyle::kToggleTwo);
     addAndMakeVisible(mute);
 
