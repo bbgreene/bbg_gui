4d0dcb5c354c6fe7b50782c826f5014ca6fbb369
bbgreene <brian.greene.music@gmail.com>
2022-05-23 11:40:46 +0200

    changed pushButton default colours

--- Source/PushButton.cpp
+++ Source/PushButton.cpp
@@ -15,9 +15,14 @@ void bbg_gui::bbg_PushButton::initProperties(juce::String labelText)
     setLookAndFeel(&customButton);
     setButtonText(labelText);
     setClickingTogglesState(true);
-    setColour(0x1000100, juce::Colours::black.brighter(0.1));
-    setColour(0x1000c00, juce::Colours::whitesmoke.withAlpha(0.06f));
-    setColour(0x1000101, juce::Colours::lightgoldenrodyellow.darker(0.2f));
-    setColour(0x1000102, juce::Colours::whitesmoke.withAlpha(0.5f));
-    setColour(0x1000103, juce::Colours::black.brighter(0.1).withAlpha(0.85f));
+    
+    setColour(juce::TextButton::ColourIds::buttonOnColourId, CustomColours::creamWhite);
+    setColour(juce::TextButton::ColourIds::buttonColourId, juce::Colours::dimgrey);
+    setColour(juce::TextButton::ColourIds::textColourOffId, juce::Colours::black);
+    setColour(juce::TextButton::ColourIds::textColourOnId, juce::Colours::black);
+//    setColour(0x1000100, juce::Colours::black.brighter(0.1));
+//    setColour(0x1000c00, juce::Colours::whitesmoke.withAlpha(0.06f));
+//    setColour(0x1000101, juce::Colours::lightgoldenrodyellow.darker(0.2f));
+//    setColour(0x1000102, juce::Colours::whitesmoke.withAlpha(0.5f));
+//    setColour(0x1000103, juce::Colours::black.brighter(0.1).withAlpha(0.85f));
 }
