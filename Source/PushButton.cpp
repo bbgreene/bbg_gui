3b1213e6e2066160cefc592de2598556d1ce6b77
bbgreene <brian.greene.music@gmail.com>
2022-05-26 12:01:08 +0200

    pushButton colours

--- Source/PushButton.cpp
+++ Source/PushButton.cpp
@@ -19,10 +19,10 @@ void bbg_gui::bbg_PushButton::initProperties(juce::String labelText)
     setColour(juce::TextButton::ColourIds::buttonOnColourId, CustomColours::blue.brighter());
     setColour(juce::TextButton::ColourIds::buttonColourId, CustomColours::creamWhite);
     setColour(juce::TextButton::ColourIds::textColourOffId, CustomColours::black);
-    setColour(juce::TextButton::ColourIds::textColourOnId, CustomColours::creamWhite);    
-    setColour(0x1000c00, CustomColours::creamWhite.darker()); // outline colour ID
+    setColour(juce::TextButton::ColourIds::textColourOnId, CustomColours::creamWhite.brighter());
+    setColour(0x1000c00, CustomColours::blue.darker()); // outline colour ID
     
-//    setColour(0x1000100, juce::Colours::black.brighter(0.1));
+//    setColour(0x1000100, CustomColours::blackGrey);
 //    setColour(0x1000101, juce::Colours::lightgoldenrodyellow.darker(0.2f));
 //    setColour(0x1000102, juce::Colours::whitesmoke.withAlpha(0.5f));
 //    setColour(0x1000103, juce::Colours::black.brighter(0.1).withAlpha(0.85f));
